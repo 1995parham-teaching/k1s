8db07d5b963cf99ee8bed676ee7e8b37bee5e393
Parham Alvani <parham.alvani@gmail.com>
2019-05-05 00:01:10 +0430

    Adds hello server.

---
+++ hello-server/main.go
@@ -0,0 +1,58 @@
+/*
+ *
+ * In The Name of God
+ *
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 04-05-2019
+ * |
+ * | File Name:     main.go
+ * +===============================================
+ */
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	fmt.Println("18.20 at Sep 07 2016 7:20 IR721")
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		logrus.Errorf("Cannot detect host name: %s", err)
+		hostname = "parham"
+	}
+
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, fmt.Sprintf("Say hello from %s to whom left me alone many years ago", hostname))
+	})
+	if err := e.Start(":1372"); err != nil && err != http.ErrServerClosed {
+		logrus.Fatalf("Server startup failed: %s", err)
+	}
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	fmt.Println("18.20 As always ... left me alone")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		log.Printf("API Service failed on exit: %s", err)
+	}
+}
