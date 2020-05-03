65772b2c0faf498717b54ea7c0cc286a187de03a
Parham Alvani <parham.alvani@gmail.com>
2020-05-03 11:54:41 +0430

    feat: Update Project Structure

---
+++ hello-server/cmd/server/main.go
@@ -0,0 +1,73 @@
+/*
+ *
+ * In The Name of God
+ *
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 04-05-2019
+ * |
+ * | File Name:     main.go
+ * +===============================================
+ */
+
+package server
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+// ShutdownTimeout is a time for shutting down the echo server
+const ShutdownTimeout = 5 * time.Second
+
+func main() {
+	hostname, err := os.Hostname()
+	if err != nil {
+		logrus.Errorf("Cannot detect host name: %s", err)
+
+		hostname = "parham"
+	}
+
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, fmt.Sprintf("Say hello from %s to Raha", hostname))
+	})
+	e.GET("/healthz", func(c echo.Context) error {
+		return c.NoContent(http.StatusNoContent)
+	})
+
+	if err := e.Start(":1378"); err != nil && err != http.ErrServerClosed {
+		logrus.Fatalf("Server startup failed: %s", err)
+	}
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), ShutdownTimeout)
+	defer cancel()
+
+	if err := e.Shutdown(ctx); err != nil {
+		logrus.Errorf("API Service failed on exit: %s", err)
+	}
+}
+
+func Register(root *cobra.Command) {
+	root.AddCommand(&cobra.Command{
+		Use:   "server",
+		Short: "Run the hello server",
+		Run: func(cmd *cobra.Command, args []string) {
+			main()
+		},
+	})
+}
